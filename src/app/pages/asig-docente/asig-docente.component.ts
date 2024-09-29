0bb0507dc3670bac402910344100b8e480358064
rSaezP <ro.saezp@duocuc.cl>
2024-09-29 11:53:22 -0300

    pantallas con css

--- src/app/pages/asig-docente/asig-docente.component.ts
+++ src/app/pages/asig-docente/asig-docente.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from 'src/app/servicios/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-asig-docente',
   templateUrl: './asig-docente.component.html',
   styleUrls: ['./asig-docente.component.scss'],
 })
 export class AsigDocenteComponent  implements OnInit {
+  asignatura: string = '';
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.asignatura = params.get('asignatura') || 'Asignatura no definida'; // Obtener el valor del parámetro asignatura
+    });
+  }
+  simularGenerarQR() {
+    console.log('Simulación: Código QR generado.');
+    // Aquí podrías mostrar una alerta o algo visual si lo deseas
+  }
 
+logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']); // Redirigir al login después de cerrar sesión
+  }
+  volver() {
+    this.router.navigate(['/docente']); // Redirigir a la pantalla de docente
+  }
 }
