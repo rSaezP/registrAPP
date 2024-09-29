12e5e8e7c259fc76759644dd0db62c0117f7497f
rSaezP <ro.saezp@duocuc.cl>
2024-09-28 23:31:00 -0300

    cambios realizados en el nuevo directorio

--- src/app/pages/recuperar/recuperar.component.ts
+++ src/app/pages/recuperar/recuperar.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-recuperar',
   templateUrl: './recuperar.component.html',
   styleUrls: ['./recuperar.component.scss'],
 })
 export class RecuperarComponent  implements OnInit {
+  email: string = ''; // Inicializa la variable
 
-  constructor() { }
+  constructor(private router: Router) {}
+
+  enviarSolicitud() {
+    // Aquí puedes realizar otras acciones necesarias antes de redirigir
+    this.email = ''; // Limpia el campo de email
+    this.router.navigate(['/login']); // Redirige a login
+  }
+
+  // Método para validar el email
 
   ngOnInit() {}
 
