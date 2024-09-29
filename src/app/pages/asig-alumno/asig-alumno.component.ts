4479afa236137d919872e6734787156213373ac9
rSaezP <ro.saezp@duocuc.cl>
2024-09-29 12:35:22 -0300

    validar email en recuperar contraseña

--- src/app/pages/asig-alumno/asig-alumno.component.ts
+++ src/app/pages/asig-alumno/asig-alumno.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-asig-alumno',
@@ -6,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asig-alumno.component.scss'],
 })
 export class AsigAlumnoComponent  implements OnInit {
+  asignatura: string = '';
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.asignatura = params.get('asignatura') || 'Asignatura no definida'; // Obtener el valor del parámetro asignatura
+    });
+  }
+  simularEscanearQR() {
+    console.log('Simulación: Código QR generado.');
+    // Aquí podrías mostrar una alerta o algo visual si lo deseas
+  }
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']); // Redirigir al login después de cerrar sesión
+  }
+  volver() {
+    this.router.navigate(['/alumno']); // Redirigir a la pantalla de docente
+  }
 
 }
